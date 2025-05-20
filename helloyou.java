f0dffee91dfc9bea46e8b1722b36d421d06cfad2
JasmineAutumn <angie.vivi2013@outlook.com>
2025-05-20 15:05:39 +0800

    add changes

--- helloyou.java
+++ helloyou.java
@@ -1,6 +1,12 @@
 public class helloyou {
       public static void main(String[] args) {
           System.out.println("Hello, World!");
+          System.out.println("Today is a great day to code!");
 
       }  
 }
+
+//this is a short comment
+/* This is a longer comment.
+  Can be used for multiple lines.
+  */
